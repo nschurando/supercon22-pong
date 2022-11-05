17dea9d64aad10700ae1e23805a8cb75f28041cc
Nicolas Schurando <nicolas@schurando.fr>
2022-11-04 17:59:41 -0700

    Cleared led matrix.

--- badge/src/main.asm
+++ badge/src/main.asm
@@ -1,6 +1,10 @@
+	; Move led matrix over to page 2
+	mov r0, 2
+	mov [0xF0], r0
+	
+	; Send letter A repeatedly
 	mov r2, 0b0100               ; High nibble of ASCII "A"
 	mov r3, 0b0001               ; Low nibble of ASCII "A"
-	
 	mov r0, r2                   ; Write high nibble to UART
 	mov [0xF7], r0               ;
 	mov r0, r3                   ; Write low nibble to UART
